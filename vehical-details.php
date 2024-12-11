cccc20ece43206a7ad2fea4d8d4c4d87f2787fda
Fairuz Tasnim <147263259+Fairuz-Tasnim@users.noreply.github.com>
2024-12-11 18:20:51 +0600

    Update vehical-details.php

--- vehical-details.php
+++ vehical-details.php
@@ -47,7 +47,7 @@ echo "<script>alert('Something went wrong. Please try again');</script>";
 <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
 <!--Custome Style -->
 <link rel="stylesheet" href="assets/css/style.css" type="text/css">
-<!--OWL Carousel slider-->
+<!--OWL Carousel slider-- >
 <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
 <link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css">
 <!--slick-slider -->
